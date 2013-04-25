8c865f1d70be764e55d76ee425686b93305cff6a
Jasper Blues <jasper@apssquick.ly>
2013-04-25 09:16:39 +0800

    Remove deps.

---
+++ Pods/Pods-bureaucratTests-header.h
@@ -0,0 +1,6 @@
+// WARNING: This feature of CocoaPods is present for discussion purposes and might be discontinued or changed in future
+#define __COCOA_PODS
+
+#define __POD_Expecta
+#define __POD_OCHamcrest
+#define __POD_OCMockito
